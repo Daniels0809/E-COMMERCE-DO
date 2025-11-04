190f12e8ecc40482af8c5b5e83df32bfaeff3739
Soy Daniel <do7269301@gmail.com>
2025-11-04 00:15:56 -0500

    terminar

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/dashboard", "/products"] }
+export const config = { matcher: ["/dashboard", "/products", "/about"] }
